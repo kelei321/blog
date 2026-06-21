697a51c880d17514844036f9032bd924fb9bad5e
kelei321 <51449002+kelei321@users.noreply.github.com>
2026-06-21 20:57:16 +0800

    fix: use esm import in tailwind config

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -1,3 +1,5 @@
+import typography from "@tailwindcss/typography";
+
 /** @type {import('tailwindcss').Config} */
 export default {
   darkMode: "class",
@@ -13,5 +15,5 @@ export default {
       },
     },
   },
-  plugins: [require("@tailwindcss/typography")],
+  plugins: [typography],
 };
